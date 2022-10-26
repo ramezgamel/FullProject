b9c30833a3a7aab90419b7b31edb4a32838ea27d
ramezgamel <98866573+ramezgamel@users.noreply.github.com>
2022-10-26 04:50:45 -0700

    Update src.js

--- BackEnd/app/src.js
+++ BackEnd/app/src.js
@@ -6,11 +6,13 @@ require("./dateBase/connect");
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors")
 
 //Settings
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(cors())
 
 //Routes
 const userRoutes = require("./routes/user.routes");
